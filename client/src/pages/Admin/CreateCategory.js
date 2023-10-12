26ff788a0a8d7e69242e1b251a502e163ccd1920
HARSHHARSANI <harshharsani007@gmail.com>
2023-10-12 16:26:04 +0530

    CraeteCategory function is up and working from the frontend must revist this function

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,8 +1,52 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout/layout";
 import AdminMenu from "../../components/layout/AdminMenu";
+import axios from "axios";
+import toast from "react-hot-toast";
+import CategoryForm from "../../components/Form/CategoryForm";
 
 const CreateCategory = () => {
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+
+  ////handle form
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/v1/category/create-category", {
+        name,
+      });
+
+      if (data.success) {
+        toast.success(`${name} is Created`);
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something Went Wrong In Input Form");
+    }
+  };
+
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+
+      if (data.success) {
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something Went Wrong");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
   return (
     <Layout title={"Create-Catagory Ecommerce-App"}>
       <div className="container-fluid m-3 p-3">
@@ -12,6 +56,33 @@ const CreateCategory = () => {
           </div>
           <div className="col-md-9">
             <h1>Manage Category</h1>
+            <div className="p-3 w-50">
+              <CategoryForm
+                handleSubmit={handleSubmit}
+                value={name}
+                setValue={setName}
+              />
+            </div>
+            <div className="w-75">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {categories?.map((c) => (
+                    <tr>
+                      <td key={c._id}>{c.name}</td>
+                      <td>
+                        <button className="btn btn-primary">Edit</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
